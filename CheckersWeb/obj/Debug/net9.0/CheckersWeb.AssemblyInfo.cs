faeb47a8ff62a47522e2d51e35b91d8bddc2133e
danSayag <151065171+danSayag@users.noreply.github.com>
2026-03-20 10:48:21 +0200

    fgdg
    dfgd

--- CheckersWeb/obj/Debug/net9.0/CheckersWeb.AssemblyInfo.cs
+++ CheckersWeb/obj/Debug/net9.0/CheckersWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckersWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1ee83700b8e79f60a377358f79c9ae91b79807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ed3070ab4c4d49c888bf1d7abe51f53c53733a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckersWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckersWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
